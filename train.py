5589d3874ec42fde5f0eda1dbbf8e0218fbfe440
ousef777 <140394809+ousef777@users.noreply.github.com>
2024-03-21 21:52:56 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,80 @@
+# Import necessary libraries 
+import torch 
+from PIL import Image 
+from torchvision import transforms
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Define a transform to convert PIL  
+# image to a Torch tensor 
+transform = transforms.Compose([ 
+    transforms.ToTensor(),
+    transforms.Normalize((0.4915, 0.4823, 0.4468), (0.2470, 0.2435, 0.2616))
+])
+
+birds = []
+planes = [] 
+for i in range(5):
+    # Read a PIL image 
+    bird_image = Image.open(f'Images/birds/bird{i}.png')
+    plane_image = Image.open(f'Images/planes/plane{i}.png')  
+
+    # Convert the PIL image to Torch tensor 
+    birds.append((transform(bird_image), 1))
+    planes.append((transform(plane_image), 0)) 
+
+cut = 3  
+dataset = birds[:cut] + planes[:cut]
+dataset_eval = birds[cut:] + planes[cut:]
+# print the converted Torch tensor 
+#print(birds[0])
+
+label_map = {0: 0, 1: 1}
+class_names = ['plane', 'bird']
+
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=False)  #batch_size=10, 
+
+model = nn.Sequential(
+            nn.Linear(3072, 32),
+            nn.Tanh(),
+            nn.Linear(32, 2),
+            nn.LogSoftmax(dim=1))
+
+learning_rate = 1e-2
+
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+loss_fn = nn.NLLLoss()
+
+n_epochs = 10
+
+for epoch in range(n_epochs):
+    for imgs, labels in train_loader:
+        outputs = model(imgs.view(imgs.shape[0], -1))
+        loss = loss_fn(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch % 1 == 0):
+      print("Epoch: %d, Loss: %.5f" % (epoch+1, float(loss)))
+      #print("%.5f" % float(loss))
+
+
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True)
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for imgs, labels in train_loader:
+        outputs = model(imgs.view(imgs.shape[0], -1))
+        _, predicted = torch.max(outputs, dim=1)
+        total += labels.shape[0]
+        #print(predicted, labels)
+        correct += int((predicted == labels).sum())
+
+print("Accuracy: %.3f" % (correct / total))
